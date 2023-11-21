b641f18cbbe9bf19f5cb3837ac3ef5e39711d1b1
bruh <quangtrung02hn16@gmail.com>
2023-11-21 16:54:28 +0700

    Update/migrate to theming-preference-integration

--- custom-preferences-theming-integration/build.gradle.kts
+++ custom-preferences-theming-integration/build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     api(project(":custom-preferences"))
-    api("com.github.unbiaseduser:theming:1.1.5")
+    api("com.github.unbiaseduser.theming:theming-preference-integration:2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
