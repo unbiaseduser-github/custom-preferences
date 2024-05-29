604f45eb450b6526a34ad380b58779a7c6a579a2
bruh <quangtrung02hn16@gmail.com>
2024-05-30 01:08:07 +0700

    Add docs

--- custom-preferences/src/main/java/com/sixtyninefourtwenty/custompreferences/CanSetPreferenceChangeListener.kt
+++ custom-preferences/src/main/java/com/sixtyninefourtwenty/custompreferences/CanSetPreferenceChangeListener.kt
@@ -1,5 +1,15 @@
 package com.sixtyninefourtwenty.custompreferences
 
+import androidx.preference.Preference
+
+/**
+ * Interface that denotes a [Preference] subclass that has a type-safe wrapper for [Preference.setOnPreferenceChangeListener].
+ * This is only for convenience of users of these classes, as they can simply look at the class
+ * declaration to see what type of object to set a listener on.
+ *
+ * Implementation notes: In [setOnPreferenceChange], call [Preference.setTypedPreferenceChangeListener].
+ * That will internally use [Preference.setOnPreferenceChangeListener].
+ */
 interface CanSetPreferenceChangeListener<T> {
     fun setOnPreferenceChange(block: ((newValue: T) -> Boolean)?)
 }
