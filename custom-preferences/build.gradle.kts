c543b5a8e03fcfa2229cb7400908fab22d32dbfe
bruh <quangtrung02hn16@gmail.com>
2024-05-30 09:29:27 +0700

    Bump stuff

--- custom-preferences/build.gradle.kts
+++ custom-preferences/build.gradle.kts
@@ -77,9 +77,9 @@ afterEvaluate {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     api("androidx.preference:preference-ktx:1.2.1")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
     implementation(project(":colorpicker"))
