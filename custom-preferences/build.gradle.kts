904b2dd69913b5786f5584a69372bb277fe8d820
bruh <quangtrung02hn16@gmail.com>
2024-08-26 22:17:07 +0700

    Bump version

--- custom-preferences/build.gradle.kts
+++ custom-preferences/build.gradle.kts
@@ -35,7 +35,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.sixtyninefourtwenty"
                 artifactId = "custom-preferences"
-                version = "2.2.0"
+                version = "2.2.2"
 
                 from(components["release"])
 
